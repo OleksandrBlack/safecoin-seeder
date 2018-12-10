ab9bd3967639c2824439b3d8052abcfbf563fbd3
OleksandrBlack <aleksandr.chornuy@gmail.com>
2018-12-10 18:18:50 +0200

    minimum requirements PROTOCOL_VERSION = 170018

--- coin.h
+++ coin.h
@@ -8,14 +8,17 @@
 #include <string>
 
 static const std::string mainnet_seeds[] = {"dnsseed.ipv6admin.com",
-                                            "dnsseedua.local.support",
-                                            "140.82.45.57",
-                                            "8.12.22.254",
-                                            "140.82.11.189",
-                                            "185.20.184.51",
-                                            "176.107.179.32",
-                                            "45.63.13.60",
-                                            ""};
+											"dnsseedua.local.support",
+											"dnsseed.fair.exchange",
+											"explorer.safecoin.org",
+											"explorer.deepsky.space",
+											"140.82.45.57",
+											"8.12.22.254",
+											"140.82.11.189",
+											"185.20.184.51",
+											"176.107.179.32",
+											"45.63.13.60",
+											""};
 
 static const std::string testnet_seeds[] = {""};
 
@@ -25,8 +28,8 @@ static const int testnet_port = 18770;
 static unsigned char pchMessageStart[4] = { 0xF1, 0xED, 0xE2, 0x8F };
 static unsigned char pchMessageStart_testnet[4] = { 0x5B, 0x1E, 0x7F, 0x63 };
 
-#define REQUIRE_VERSION 170017
-static const int minimunClientVersion = 170017;
-static const int PROTOCOL_VERSION = 170019;
+#define REQUIRE_VERSION 170018
+static const int minimunClientVersion = 170018;
+static const int PROTOCOL_VERSION = 170019; //Upcoming SafeNodes Update
 static const int INIT_PROTO_VERSION = 212; //Safecoin
 #endif // __INCLUDED_COIN_H__
